a6de5ecbbc2e6140d55d608483731e148359de8c
SandeepMS07 <sandeepms746@gmail.com>
2022-03-31 16:02:06 +0530

    responsive design using next.js

--- components/PizzaCard.jsx
+++ components/PizzaCard.jsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 const PizzaCard = () => {
   return (
     <div className={styles.container}>
-      <Image src="/img/pizza.png" alt="" width="500" height="500" />
+      <Image src="/img/pizza.png" alt="" width="300" height="300" />
       <h1 className={styles.title}>FIORI DI ZUCCA</h1>
       <span className={styles.price}>$19.90</span>
       <p className={styles.desc}>
